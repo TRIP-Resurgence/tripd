306775c32a47de9e26d7add1df41c0594c7be6f6
arf20 <aruizfernandez05@gmail.com>
2025-12-17 01:28:12 +0100

    Commence peer session logic

---
+++ src/lsfunctions/ls_peer_session.h
@@ -0,0 +1,29 @@
+/*
+
+    trip: Modern TRIP LS implementation
+    Copyright (C) 2023 arf20 (Ángel Ruiz Fernandez)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+*/
+
+#ifndef _LS_PEER_SESSION_H
+#define _LS_PEER_SESSION_H
+
+typedef struct {
+    int fd;
+} ls_peer_session_t;
+
+#endif /* _LS_PEER_SESSION_H */
+
